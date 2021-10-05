13ee7678e58213c8be654ababe790a425dcab01c
Skipancho <ctg1770a@gmail.com>
2021-10-05 16:00:15 +0900

    first commit

---
+++ app/src/main/java/com/e/kotlin_test1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.e.kotlin_test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_1.text = "Hello Kotlin"
+
+
+
+    }
+}
