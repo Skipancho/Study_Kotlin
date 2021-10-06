c494f214836bbc3fe057d2ed9d96e4ebd2d7ffb8
Skipancho <ctg1770a@gmail.com>
2021-10-06 22:16:46 +0900

    버튼 추가 + 자바 연동

---
+++ app/src/main/java/com/e/kotlin_test1/DaeguFoodActivity.java
@@ -0,0 +1,111 @@
+package com.e.kotlin_test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class DaeguFoodActivity extends AppCompatActivity {
+
+    private TextView result_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daegu_food);
+
+        result_tv = findViewById(R.id.result_tv);
+        result_tv.setMovementMethod(new ScrollingMovementMethod());
+        new BackgroundTask("달서구").execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void,String,Boolean> {
+        String target;
+        String s_text;
+
+        public BackgroundTask(String text){
+            this.s_text = text;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            try {
+                target="https://www.daegufood.go.kr/kor/api/tasty.html?mode=json&addr="+ URLEncoder.encode(s_text,"UTF-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try{
+                URL url = new URL(target);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                InputStream inputStream = httpsURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((temp=bufferedReader.readLine()) != null){
+                    stringBuilder.append(temp+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpsURLConnection.disconnect();
+                String result = stringBuilder.toString().trim();
+                result = result.replace(", }","}");
+                result = result.replace("\"\"","\"");
+                result = result.replace(":\",",":\"\",");
+                result = result.replaceAll("^[ |ㄱ-ㅎ|ㅏ-ㅣ|가-힣]\"[ |ㄱ-ㅎ|ㅏ-ㅣ|가-힣]","");
+
+                for(int i = 0; i < 30; i++){
+                    System.out.print(result.charAt(100833+i));
+                }
+                System.out.println();
+                for(int i = 0; i < 30; i++){
+                    System.out.print(result.charAt(18230+i));
+                }
+                System.out.println();
+                String name, menu,address;
+                int count = 0;
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                while(count<jsonArray.length()){
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    name = object.getString("BZ_NM");
+                    menu = object.getString("MNU");
+                    address = object.getString("GNG_CS");
+                    publishProgress(name,menu,address);
+                    count++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+        @Override
+        public void onProgressUpdate(String... values){
+            String name , address, menu;
+            name = values[0];
+            menu = values[1];
+            address = values[2];
+            result_tv.append(name+":"+address+"\n"+menu+"\n");
+        }
+        @Override
+        public void onPostExecute(Boolean result){
+
+        }
+    }
+}
--- app/src/main/java/com/e/kotlin_test1/MainActivity.kt
+++ app/src/main/java/com/e/kotlin_test1/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.e.kotlin_test1
 
+import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val listener : View.OnClickListener = View.OnClickListener {
+            when{
+                it.id == R.id.btn_1 -> result_tv.text = "button 1 action"
+                it.id == R.id.btn_2 -> startActivity(Intent(this,DaeguFoodActivity::class.java))
+            }
+        }
+
         tv_1.text = "Hello Kotlin"
-
-
-
+        btn_1.setOnClickListener(listener)
+        btn_2.setOnClickListener(listener)
     }
 }
