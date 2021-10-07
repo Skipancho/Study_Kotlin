772480d9eb19bfd4dbf0f3fd7bde46655459010c
Skipancho <ctg1770a@gmail.com>
2021-10-07 20:44:09 +0900

    EditText 추가

--- app/src/main/java/com/e/kotlin_test1/MainActivity.kt
+++ app/src/main/java/com/e/kotlin_test1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,15 +24,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        result_tv.movementMethod = ScrollingMovementMethod()
+
         val listener : View.OnClickListener = View.OnClickListener {
             when{
                 it.id == R.id.btn_1 -> result_tv.text = "button 1 action"
                 it.id == R.id.btn_2 -> startActivity(Intent(this,DaeguFoodActivity::class.java))
+                it.id == R.id.btn_3 -> writeText()
             }
         }
 
         tv_1.text = "Hello Kotlin"
+
         btn_1.setOnClickListener(listener)
         btn_2.setOnClickListener(listener)
+        btn_3.setOnClickListener(listener)
+    }
+
+    private fun writeText(){
+        var text = et_1.text.toString()
+        result_tv.append(text+"\n")
     }
 }
